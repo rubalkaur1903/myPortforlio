792d44bb47b4f67fc41c6073a216e0606a9b9620
Rubal Kaur <84761627+rubalkaur1903@users.noreply.github.com>
2022-05-24 15:53:51 -0700

    added Intro and added Profile.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 
-import { Home } from '../components'
+import { Home, Profile } from '../components'
 
 const App = () => {
     return (
         <BrowserRouter>
             <div>
                 <div className="navbar">
-                    <Link className="main-link" to='/'>HOME</Link>
+                    <Link className="main-link" to='/'>INTRO</Link>||
+                    <Link className="main-link" to='/profile'>PROFILE</Link>
                 </div>
                 <Routes>
-                    <Route path="/" element={<>
-                        <Home />
-                    </>} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
                 </Routes>
             </div>
         </BrowserRouter>
