e06488ce3702b32164caaafbfba6b35270f585e2
Rubal Kaur <84761627+rubalkaur1903@users.noreply.github.com>
2022-05-27 00:20:24 -0700

    backround change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Contact from './Contact'
-import img1 from '../images/IMG_6526 2.jpg'
+import img1 from '../images/IMG_6526 6.jpg'
 
 const Intro = () => {
     return (
@@ -18,7 +18,7 @@ const Intro = () => {
             <div className="summary">
                 <h3 className="about-heading">About</h3>
                 <div className="about-elements">
-                    <img className="intro-image" src={img1}></img>
+                    <img className="intro-image" src={img1} width="400px" height="300px"></img>
                     <div className="about-p-elem">
                         <p>A woman, an immigrant, a dreamer, and a passionate Web Developer with a non-traditional background seeking new opportunities.</p>
                         <p>I am a self-taught coder and a 26 week Fullstack immersive Bootcamp graduate from Fullstack Academy. I love fixing bugs and developing applications using my favorite technologies. When I am not coding, I like to hit the beach, catch up on my favorite shows, read, or spend quality time with my family and friends.</p>
