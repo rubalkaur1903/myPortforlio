712222ab633a90ee9f52229b2006092585c20b3d
Rubal Kaur <84761627+rubalkaur1903@users.noreply.github.com>
2022-06-22 01:04:49 -0700

    fixed header

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,14 @@ const App = () => {
     return (
         <BrowserRouter>
             <div>
-                <div className="navbar">
-                    <Link className="main-link" to='/'>INTRO</Link>
-                    <Link className="main-link" to='/profile'>PROFILE</Link>
+                <div className="header">
+                    <div className="header-left">
+                        <p>Portfolio</p>
+                    </div>
+                    <div className="navbar">
+                        <Link className="main-link" to='/'>INTRO</Link>
+                        <Link className="main-link" to='/profile'>PROFILE</Link>
+                    </div>
                 </div>
                 <Routes>
                     <Route exact path="/" element={<Intro />} />
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,26 +1,26 @@
 import React from "react";
 
 import Contact from './Contact'
-import img1 from '../images/IMG_6526 6.jpg'
+import img1 from '../images/IMG_4283 2.jpg'
 
 const Intro = () => {
     return (
         <div className="intro-container">
             <div className="intro">
+                <div>
+                    <img className="intro-image" src={img1} width="380px" height="400px"></img>
+                </div>
                 <div className="intro-left">
                     <p className="intro-left-name">Rubal Kaur</p>
                     <p className="intro-left-title">Web Developer</p>
-                </div>
-                <div className="intro-right">
-                    <p>Portfolio</p>
                 </div>
             </div>
             <div className="summary">
                 <h3 className="about-heading">About</h3>
                 <div className="about-elements">
-                    <img className="intro-image" src={img1} width="400px" height="300px"></img>
+                    {/* <img className="intro-image" src={img1} width="400px" height="300px"></img> */}
                     <div className="about-p-elem">
-                        <p>A woman, an immigrant, a dreamer, and a passionate Web Developer with a non-traditional background seeking new opportunities.</p>
+                        <p>A passionate Web Developer with a non-traditional background seeking new opportunities.</p>
                         <p>I am a self-taught coder and a 26 week Fullstack immersive Bootcamp graduate from Fullstack Academy. I love fixing bugs and developing applications using my favorite technologies. When I am not coding, I like to hit the beach, catch up on my favorite shows, read, or spend quality time with my family and friends.</p>
                     </div>
                 </div>
